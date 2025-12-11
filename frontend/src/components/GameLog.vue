178f99ec5bc1aa433f25465a2b9dc9a4b9899560
Karel Hanák <karel.hanak@iqrf.org>
2025-12-11 19:56:00 +0100

    GameLog: scroll log to bottom if needed
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- frontend/src/components/GameLog.vue
+++ frontend/src/components/GameLog.vue
@@ -1,6 +1,6 @@
 <template>
   <label>{{ $t('sections.log') }}</label>
-  <div class="log">
+  <div id="logContainer" class="log">
     <div
       v-for="(log, index) in logs"
       :key="index"
@@ -13,7 +13,20 @@
 <script setup lang="ts">
 import { useLogStore } from '@/stores/logStore';
 import { storeToRefs } from 'pinia';
+import { nextTick, watch } from 'vue';
 
 const logStore = useLogStore();
 const { logs } = storeToRefs(logStore);
+
+watch(() => logs.value.length, async () => {
+  const logDiv = document.getElementById('logContainer');
+  console.log(logDiv);
+  if (!logDiv) {
+    return;
+  }
+  if (logDiv.scrollHeight > logDiv.clientHeight) {
+    await nextTick();
+    logDiv.scrollTop = logDiv.scrollHeight;
+  }
+});
 </script>
